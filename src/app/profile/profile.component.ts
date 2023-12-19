ff9f9ba33bb76b1f9fe9d6f61c153e9bdf826267
ArdraH <ardra.h@thinkpalm.com>
2023-12-19 18:33:41 +0530

    angular

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,7 +4,7 @@ import { ApiService } from '../api.service';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../environments/environment.development';
 import { error, log } from 'console';
-import { User } from '../models/data-types';
+import { Blog, User } from '../models/data-types';
 
 @Component({
   selector: 'app-profile',
@@ -17,17 +17,36 @@ export class ProfileComponent implements OnInit {
   [x: string]: any;
   userid:number|any;
   username:string| any;
+  user:string|any
 email:any;
 bio: string|any;
+blogDetails:Blog[]|any=[]
 users:User[] |any;
 constructor(private api:ApiService,private activatedRoute:ActivatedRoute){}
 ngOnInit(): void {
   this.api.getReturn(`${environment.BASE_API_URL}/auth/userProfile`).subscribe((data)=>{
     this.users=data
     console.log(this.users);
+    this.fetchUserBlogPost()
   },(error)=>{
     console.log(error);
-  })
+  }
+  );
+}
+fetchUserBlogPost() {
+  this.user=localStorage.getItem("user")
+    this.userid=JSON.parse(this.user).id;
+  if (this.users && this.users.id) {
+    this.api.getReturn(`${environment.BASE_API_URL}/auth/${this.users.id}/blogposts`).subscribe(
+      (blogpost) => {
+        console.log("User Blog Post:", blogpost);
+        this.blogDetails = blogpost;
+      },
+      (error) => {
+        console.log("Error fetching blog posts", error);
+      }
+    );
+  }
 }
 
 
